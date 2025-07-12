c60628687c1415a6657e0907e98b7a688eceb151
Laboratorio DTI - PUCP <aml@pucp.edu.pe>
2025-07-11 21:50:45 -0500

    commit

--- iac/modules/events/main.tf
+++ iac/modules/events/main.tf
@@ -1,11 +1,11 @@
-resource "aws_cloudwatch_event_bus" "citaes_bus" {
+resource "aws_cloudwatch_event_bus" "citas_bus" {
     name = "citaes-bus"
 }
 
 resource "aws_cloudwatch_event_rule" "crear_cita" {
     name           = "crear-cita"
     description    = "Regla para crear cita desde evento personalizado"
-    event_bus_name = aws_cloudwatch_event_bus.citaes_bus.name
+    event_bus_name = aws_cloudwatch_event_bus.citas_bus.name
     event_pattern = jsonencode({
         source       = ["pe.com.veterinariavirtual"],
         "detail-type": ["crear-cita"]
@@ -16,7 +16,7 @@ resource "aws_cloudwatch_event_target" "target_lambda_crear_cita" {
     rule      = aws_cloudwatch_event_rule.crear_cita.name
     target_id = "crear-cita-lambda"
     arn       = var.crear_cita_funcion_arn
-    event_bus_name = aws_cloudwatch_event_bus.citaes_bus.name
+    event_bus_name = aws_cloudwatch_event_bus.citas_bus.name
 }
 
 resource "aws_lambda_permission" "allow_eventbridge" {
