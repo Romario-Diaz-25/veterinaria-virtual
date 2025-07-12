c975c7b96bfa1dbb4789dab7d939bea9b99605f2
Laboratorio DTI - PUCP <aml@pucp.edu.pe>
2025-07-11 22:06:03 -0500

    commit

--- veterinaria/src/main/java/pe/com/veterinaria/controller/CitaMedicaController.java
+++ veterinaria/src/main/java/pe/com/veterinaria/controller/CitaMedicaController.java
@@ -1,6 +1,5 @@
 package pe.com.veterinaria.controller;
 
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pe.com.veterinaria.modelo.CitaMedica;
